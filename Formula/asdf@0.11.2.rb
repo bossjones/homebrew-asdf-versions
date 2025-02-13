00a43283161c448162c665419914f3dedea92d12
Malcolm Jones <bossjones@theblacktonystark.com>
2025-02-12 22:49:13 -0500

    Update asdf Homebrew formula with improved installation and testing
    - Add dependencies for better compatibility
    - Refactor installation process to use Homebrew conventions
    - Enhance completions installation
    - Update test cases to verify version and plugin management
    - Improve caveats with shell-specific instructions

--- Formula/asdf@0.11.2.rb
+++ Formula/asdf@0.11.2.rb
@@ -5,32 +5,43 @@ class AsdfAT0112 < Formula
   sha256 "8fb63f069d4c5b12778b2749652a7c059030dab5e231f0a2a03270037a3705fd"
   license "MIT"
 
+  depends_on "autoconf"
+  depends_on "automake"
+  depends_on "coreutils"
+  depends_on "libtool"
+  depends_on "libyaml"
+  depends_on "openssl@3"
+  depends_on "readline"
+  depends_on "unixodbc"
+
   def install
-    libexec.install "bin", "lib", "LICENSE", "README.md"
-    libexec.install "completions"
-    libexec.install "defaults" => "etc"
+    bash_completion.install "completions/asdf.bash"
+    fish_completion.install "completions/asdf.fish"
+    zsh_completion.install "completions/_asdf"
+    libexec.install Dir["*"]
+    touch libexec/"asdf_updates_disabled"
 
-    bin.install_symlink libexec/"bin/asdf"
-
-    bash_completion.install_symlink libexec/"completions/asdf.bash" => "asdf"
-    fish_completion.install_symlink libexec/"completions/asdf.fish"
-    zsh_completion.install_symlink libexec/"completions/_asdf"
+    bin.write_exec_script libexec/"bin/asdf"
   end
 
   def caveats
-    <<~EOS
-      To use asdf, add the following line to your ~/.bash_profile or ~/.zshrc:
-        . "#{opt_libexec}/asdf.sh"
-    EOS
+    s = "To use asdf, add the following line to your #{shell_profile}:\n"
+
+    s += if preferred == :fish
+      "  source #{opt_libexec}/asdf.fish\n\n"
+    else
+      "  . #{opt_libexec}/asdf.sh\n\n"
+    end
+
+    s += "Restart your terminal for the settings to take effect."
+
+    s
   end
 
   test do
-    assert_match "version", shell_output("#{bin}/asdf --version")
-    system "#{bin}/asdf", "plugin", "list"
-    assert_match "Oohes!",
-      shell_output("#{bin}/asdf plugin-add bogus https://github.com/asdf-vm/asdf-bogus.git")
-    assert_match "Updated bogus to revision",
-      shell_output("#{bin}/asdf plugin-update bogus")
-    system "#{bin}/asdf", "plugin", "remove", "bogus"
+    assert_match version.to_s, shell_output("#{bin}/asdf version")
+    output = shell_output("#{bin}/asdf plugin-list 2>&1", 1)
+    assert_match "No plugins installed", output
+    assert_match "Update command disabled.", shell_output("#{bin}/asdf update", 42)
   end
 end
