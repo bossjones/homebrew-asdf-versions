412b66ff1128caafaf3a0442810f6efc2e432882
Malcolm Jones <bossjones@theblacktonystark.com>
2025-02-12 22:43:38 -0500

    Refactor asdf Homebrew formula installation and completions
    - Simplify installation process by using libexec and symlinks
    - Move completions and defaults to libexec
    - Add caveats with asdf.sh sourcing instructions
    - Remove commented-out code and redundant installation steps

--- Formula/asdf@0.11.2.rb
+++ Formula/asdf@0.11.2.rb
@@ -1,20 +1,27 @@
-class AsdfAT01112 < Formula
+class AsdfAT0112 < Formula
   desc "Extendable version manager with support for Ruby, Node.js, Elixir, Erlang & more"
   homepage "https://asdf-vm.com/"
   url "https://github.com/asdf-vm/asdf/archive/v0.11.2.tar.gz"
-  # sha256 "77b24369c9d8f2b3d2b5f0097a25d5f9ee1b0c1dbf4c4148a8c2c7c8f766b57e"
   sha256 "8fb63f069d4c5b12778b2749652a7c059030dab5e231f0a2a03270037a3705fd"
   license "MIT"
 
   def install
-    bash_completion.install "completions/asdf.bash"
-    fish_completion.install "completions/asdf.fish"
-    zsh_completion.install "completions/_asdf"
-    libexec.install "bin", "lib"
-    prefix.install "LICENSE", "README.md"
-    (prefix/"etc").install "defaults"
+    libexec.install "bin", "lib", "LICENSE", "README.md"
+    libexec.install "completions"
+    libexec.install "defaults" => "etc"
 
-    (bin/"asdf").write_env_script libexec/"bin/asdf", :ASDF_DIR => libexec
+    bin.install_symlink libexec/"bin/asdf"
+
+    bash_completion.install_symlink libexec/"completions/asdf.bash" => "asdf"
+    fish_completion.install_symlink libexec/"completions/asdf.fish"
+    zsh_completion.install_symlink libexec/"completions/_asdf"
+  end
+
+  def caveats
+    <<~EOS
+      To use asdf, add the following line to your ~/.bash_profile or ~/.zshrc:
+        . "#{opt_libexec}/asdf.sh"
+    EOS
   end
 
   test do
